19f3dda1912d0870daaa3b7ef5354be148e37086
vanviet2627 <vanviet8367@gmail.com>
2019-05-29 00:45:43 +0700

    Connect database

---
+++ configs/database.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const uri = require('../configs').uri;
+
+class Database { 
+  constructor() {
+    this._connect();
+  }
+  _connect() {
+    mongoose.connect(uri, {useNewUrlParser: true}).then(() => {
+      console.log("Database connected!");
+    }).catch(err => {
+      console.log("Database connect error: " + err);
+    })
+  }
+}
+
+module.exports = new Database;
