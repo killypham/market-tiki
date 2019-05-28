4ee82d587b952b900e4869bfcbdfaa3bdf7141df
vanviet2627 <vanviet8367@gmail.com>
2019-05-28 17:29:49 +0700

    Add route

---
+++ routes/api/books.js
@@ -0,0 +1,11 @@
+const router = require('express').Router()
+const Book = require('../../models/book.model');
+// router.get('/', (req, res, next) => {
+  
+// })
+
+router.post('/', (req, res, next) => {
+  
+})
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* API module */
+router.use('/api', require("./api"));
+
 module.exports = router;
