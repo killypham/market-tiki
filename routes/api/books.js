24941fe44c050cb79e33bdce3eb6365ef5f12b85
vanviet2627 <vanviet8367@gmail.com>
2019-05-29 01:56:47 +0700

    Update book route

--- routes/api/books.js
+++ routes/api/books.js
@@ -1,11 +1,47 @@
 const router = require('express').Router()
 const Book = require('../../models/book.model');
-// router.get('/', (req, res, next) => {
+
+// Find by book title
+router.get('/search', (req, res) => {
+  const result = Book.findByTitle(req.query.q);
+
+  result.then(books => {
+    res.status(200).json(books);
+  }).catch(err => {
+    res.status(500).json({books: null, err: err});
+  })
+})
+
+router.get('/', (req, res, next) => {
+  const result = Book.getListBooks();
   
-// })
+  result.then(books => {
+    res.status(200).json(books);
+  }).catch(err => {
+    res.status(500).json({books: null, err: err});
+  })
+})
 
 router.post('/', (req, res, next) => {
+  const newBook = req.body;
+  const savedBook = Book.addBook(newBook);
   
+  savedBook.then(book => {
+    res.status(200).json(book);
+  }).catch(err => {
+    res.status(500).json({book: null, err: err})
+  })
+})
+
+router.put('/', (req, res) => {
+  const bookInfo = req.body;
+  let result = Book.updateBookInfo(bookInfo);
+  
+  result.then(updatedBook => {
+    res.status(200).json(updatedBook);
+  }).catch(err => {
+    res.status(500).json({updatedBook: null, err: err});
+  })
 })
 
 module.exports = router;
