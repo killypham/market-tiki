c2c9a9d728d062853c2f11755e66b2ffbb4e8e4d
vanviet2627 <vanviet8367@gmail.com>
2019-05-29 16:04:16 +0700

    Update book routes

--- routes/api/books.js
+++ routes/api/books.js
@@ -1,52 +1,18 @@
 const router = require('express').Router()
 const Book = require('../../models/book.model');
 
-
+// Get all books
 router.get('/', (req, res, next) => {
-  const result = Book.getListBooks();
-  
-  result.then(books => {
+  Book.getListBooks().then(books => {
     res.status(200).json(books);
   }).catch(err => {
     res.status(500).json({books: null, err: err});
   })
 })
 
-router.post('/', (req, res, next) => {
-  const newBook = req.body;
-  const savedBook = Book.addBook(newBook);
-  
-  savedBook.then(book => {
-    res.status(200).json(book);
-  }).catch(err => {
-    res.status(500).json({book: null, err: err})
-  })
-})
-
-router.put('/', (req, res) => {
-  const bookInfo = req.body;
-  let result = Book.updateBookInfo(bookInfo);
-  
-  result.then(updatedBook => {
-    res.status(200).json(updatedBook);
-  }).catch(err => {
-    res.status(500).json({updatedBook: null, err: err});
-  })
-})
-
-router.delete('/:bookId', (req, res) => {
-  Book.deleteBookById(req.params.bookId).then(() => {
-    res.status(200).json("Book deleted");
-  }).catch(err => {
-    res.status(500).json({msg: "Cannot delete", err: err});
-  })
-})
-
-// Find book by title
+// Find books by title
 router.get('/search', (req, res) => {
-  const result = Book.findByTitle(req.query.q);
-
-  result.then(books => {
+  Book.findByTitle(req.query.q).then(books => {
     res.status(200).json(books);
   }).catch(err => {
     res.status(500).json({books: null, err: err});
@@ -55,12 +21,39 @@ router.get('/search', (req, res) => {
 
 // View book detail
 router.get('/detail', (req, res) => {
-  const result = Book.findById(req.query.bookId);
-  result.then(bookDetail => {
+  Book.findById(req.query.q).then(bookDetail => {
     res.status(200).json(bookDetail);
   }).catch(err => {
     res.status(500).json(err);
   })
 })
+
+// Add a new book
+router.post('/', (req, res, next) => {
+  Book.addBook(req.body).then(book => {
+    res.status(200).json(book);
+  }).catch(err => {
+    res.status(500).json({book: null, err: err})
+  })
+})
+
+// Edit a book
+router.post('/edit', (req, res) => {
+  Book.updateBookInfo(req.body).then(updatedBook => {
+    res.status(200).json(updatedBook);
+  }).catch(err => {
+    res.status(500).json({updatedBook: null, err: err});
+  })
+})
+
+// Delete book by book ID
+router.post('/delete/:bookId', (req, res) => {
+  Book.deleteBookById(req.params.bookId).then(() => {
+    res.status(200).json("Book deleted");
+  }).catch(err => {
+    res.status(500).json({msg: "Cannot delete", err: err});
+  })
+})
+
 
 module.exports = router;
