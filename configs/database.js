ad147d180e49dfae70b28096f0d868e143b00879
vanviet2627 <vanviet8367@gmail.com>
2019-05-29 14:31:05 +0700

    Update useFindAndModify

--- configs/database.js
+++ configs/database.js
@@ -1,12 +1,15 @@
 const mongoose = require('mongoose');
 const uri = require('../configs').uri;
 
-class Database { 
+class Database {
   constructor() {
     this._connect();
   }
   _connect() {
-    mongoose.connect(uri, {useNewUrlParser: true}).then(() => {
+    mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useFindAndModify: false
+    }).then(() => {
       console.log("Database connected!");
     }).catch(err => {
       console.log("Database connect error: " + err);
