4e57865f106640fb98b0c8b5011f796871e35891
Killypham <nguyenpham.281098@gmail.com>
2019-06-03 04:46:46 +0800

    connect database (folder configs)
    add file configs\databse.js
    ad required from db.js

---
+++ configs/database.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const uri = require('../configs').uri;
+
+class Database {
+  constructor() {
+    this._connect();
+  }
+  _connect() {
+    mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useFindAndModify: false
+    }).then(() => {
+      console.log("Database connected!");
+    }).catch(err => {
+      console.log("Database connect error: " + err);
+    })
+  }
+}
+
+module.exports = new Database;
